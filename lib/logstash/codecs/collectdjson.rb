d7838086723ef6566093a17121d65f32756c5383
Pieter Lexis <pieter@kumina.nl>
2014-02-14 15:32:22 +0100

    KISS, un-threadize the codec

--- lib/logstash/codecs/collectdjson.rb
+++ lib/logstash/codecs/collectdjson.rb
@@ -20,28 +20,26 @@ class LogStash::Codecs::CollectdJSON < LogStash::Codecs::Base
 
   public
   def decode(line)
-    Thread.new do
-      # The collectd JSON is 'wrapped' inside an array
-      collectd = JSON.parse(line[1..-2])
-      ls_event = {}
+    # The collectd JSON is 'wrapped' inside an array
+    collectd = JSON.parse(line[1..-2])
+    ls_event = {}
 
-      # Parse some special field in the data
-      ls_event['@timestamp'] = Time.at(collectd['time'])
-      ls_event['collectd_type'] = collectd['type']
-      ls_event['interval'] = collectd['interval'] if !@prune_intervals
-      collectd['dsnames'].each_with_index do |name,i|
-        ls_event[name] = collectd['values'][i]
-      end
-
-      # delete all fields from collectd already added to the event and
-      # clean up the collectd data
-      collectd.each do |key,value|
-        collectd.delete(key) if @to_delete.include?(key) or value.empty?
-      end
-
-      # We merge ls_event with collectd because collectd might contain extra
-      # data we care about
-      yield(LogStash::Event.new(ls_event.merge(collectd)))
+    # Parse some special field in the data
+    ls_event['@timestamp'] = Time.at(collectd['time'])
+    ls_event['collectd_type'] = collectd['type']
+    ls_event['interval'] = collectd['interval'] if !@prune_intervals
+    collectd['dsnames'].each_with_index do |name,i|
+      ls_event[name] = collectd['values'][i]
     end
+
+    # delete all fields from collectd already added to the event and
+    # clean up the collectd data
+    collectd.each do |key,value|
+      collectd.delete(key) if @to_delete.include?(key) or value.empty?
+    end
+
+    # We merge ls_event with collectd because collectd might contain extra
+    # data we care about
+    yield(LogStash::Event.new(ls_event.merge(collectd)))
   end
 end
